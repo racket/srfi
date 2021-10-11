c4ec2c84b6c26778a68bdf7873d0c74de736d15d
Philip McGrath <philip@philipmcgrath.com>
2021-10-11 17:35:27 -0400

    add license to package info.rkt files
    Other minor changes:
    
      - "srfi-doc/srfi/scribblings/srfi.scrbl":
        Use HTTPS for the SRFI website.
    
      - "srfi-lib-nonfree/info.rkt":
        Explicitly state in `pkg-desc` that this package is free
        software (since it is an empty package for backward
        compatibility).
    
    related to https://github.com/racket/racket/pull/3760

--- srfi-doc/info.rkt
+++ srfi-doc/info.rkt
@@ -22,3 +22,6 @@
 (define pkg-desc "documentation part of \"srfi\"")
 
 (define pkg-authors '(mflatt noel chongkai jay))
+
+(define license
+  '(Apache-2.0 OR MIT))
--- srfi-lite-lib/info.rkt
+++ srfi-lite-lib/info.rkt
@@ -7,3 +7,6 @@
 (define pkg-desc "implementation of the most widely used \"srfi\" libraries")
 
 (define pkg-authors '(mflatt))
+
+(define license
+  '(Apache-2.0 OR MIT))
